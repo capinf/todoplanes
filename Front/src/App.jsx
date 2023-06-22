5453555efdc8b5d29cb893f454e82407648a0a34
Nahuiii <cabrera.nahueloctavio@gmail.com>
2023-06-22 00:42:44 -0300

    pequeños avances

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,30 +1,35 @@
 import { NavBar } from './components/NavBar'
 import './App.css'
 import CarouselComponent from './components/Carrousel'
-import { ListOfCars } from './components/ListOfCars'
+import {Cars} from './components/Cars'
+import responseCars from './datatest/withresults.json'
+import noResults from './datatest/withoutresults.json'
 
 
 function App() {
+  const cars = responseCars.Search
+  console.log(cars)
 
   return (
     <>
+    <div className='page'>
       <header>
-      <h1>Todo Planes</h1>
-      <NavBar />
+        <h1>Todo Planes</h1>
+        <NavBar />
+        <div><CarouselComponent /></div>
       </header>
 
       <main>
-
-      <CarouselComponent />
-      <ListOfCars />
+        <section><Cars cars={cars} /></section>
 
       </main>
 
       <footer>
 
-      <h3>Documentacion + Redes sociales</h3>
+        <h3>Documentacion + Redes sociales</h3>
 
       </footer>
+      </div>
     </>
   )
 }
