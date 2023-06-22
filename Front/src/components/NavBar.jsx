af76af788c7ba3574dc210137bad7b23b8e55f9e
Nahuiii <cabrera.nahueloctavio@gmail.com>
2023-06-22 16:11:19 -0300

    agregado de search y login

--- Front/src/components/NavBar.jsx
+++ Front/src/components/NavBar.jsx
@@ -15,10 +15,16 @@ export function NavBar () {
           <Nav.Link href="#">Planes</Nav.Link>
         </Nav>
       </Navbar.Collapse>
+            <div className="logo">
+            <img src="/img/logo.png" width="120px" height="120px" alt="logo" />
+            </div>
+            <form className="form-inline my-2 my-lg-0">
+            <input className="form-control ml-sm-2" type="search" placeholder="Ford..." aria-label="Search"/>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+            </form>
+            <a className="login" href="#"><img src="/img/iniciarsesion1.png" width="50px" height="50px" alt="user" /></a>
     </Navbar>
-        <div className="logo">
-        <img src="/img/logo.png" width="120px" height="120px" alt="logo" />
-        </div>
+
         </>
     )
 }
