974105718e2e1b9ce32d1506ac613a088607278c
Nahuel Cabrera <cabrera.nahueloctavio@gmail.com>
2023-06-24 13:16:00 -0300

    grid de autos

--- Front/src/components/Home.jsx
+++ Front/src/components/Home.jsx
@@ -1,10 +1,33 @@
 import { Route, Routes } from 'react-router-dom'
 import  CarouselComponent  from './Carrousel'
+import withresults from '../datatest/withresults.json'
+import '../Css/GridAutos.css'
 
 export function Home() {
+  const Cars = withresults.Search
+  const hasCars = Cars.length > 0
+
   return (
     <>
-    <CarouselComponent/>
+    <header><CarouselComponent/></header>
+    <body>
+      <div className='gridAutos'>
+        <ul>
+        {
+        Cars.map(car =>(
+
+        <li key={car.carId}>
+        <img src={car.img} alt="" width="150px" height="150px" />
+        <h1 className='title'>{car.title}</h1>
+        <p className='info'>{car.Kilometraje}</p>
+        <p className='info'>{car.Kilometraje}</p>
+        <p className='info'>{car.descripcion}</p>
+        </li>
+        ))
+         }
+        </ul>
+      </div>
+    </body>
       <div className="App">
       </div>
     </>
